8449a325dda9b5922d896ec632dfb5b51aa899d0
acorovic1 <148489071+acorovic1@users.noreply.github.com>
2023-11-10 14:50:29 +0100

    Add files via upload

---
+++ LV4-Z12s/src/main/java/org/example/LaptopDaoSerializableFile.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class LaptopDaoSerializableFile implements LaptopDao{
+    File fajl;
+    ArrayList<Laptop>laptopi;
+
+
+
+    public void dodajLaptopUListu(Laptop laptop){}
+    public void dodajLaptopUFile(Laptop laptop){}
+    public void getLaptop(String procesor){}
+    public void napuniListu(ArrayList<Laptop> laptopi){}
+    public void vratiPodatkeIzDatoteke(){}
+}
